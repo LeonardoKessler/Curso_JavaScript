ce2572d0aaf8f0f297a1fd3ca8a08c9c91dbff64
LeonardoKessler <136763547+LeonardoKessler@users.noreply.github.com>
2023-06-15 21:56:58 -0300

    Add files via upload

---
+++ aula9.js
@@ -0,0 +1,26 @@
+/*
+em uma competicao de salto em distancia cada atleta tem direito a cinco saltos.
+No final da serie de saltos de cada atleta, o melhor e o pior resultados sao eliminados.
+O seu resultado fica sendo a media dos tres valores restantes.
+Voce deve fazer um programa que receba o nome e as cinco distancias alcancadas pelo atleta em seus saltos e
+depois informe a media dos saltos conforme a descricao acima informada (retirar o melhor e o pior salto
+e depois calcular a media).
+Faca uso de uma lista para armazenar os saltos.
+Os saltos sao informados na ordem da execucao, por tanto nao sao ordenados.
+
+a saida do programa deve ser conforme o exemplo abaixo:
+
+Atleta: Rodrigo Curvêllo
+
+Primeiro salto: 6.5 m
+Segundo salto: 6.1 m
+Terceiro salto: 6.2 m
+quarto salto: 5.4 m
+quinto salto: 5.3 m
+
+Melhor salto: 6.5 m
+Pior salto: 5.3 m
+Media dos demais saltos: 5.9 m
+
+Resultado final:
+Rodrigo Curvêllo: 5.9 m
