cd1fbc424e420f649ba7f3d3203c96da080eeaa0
LeonardoKessler <136763547+LeonardoKessler@users.noreply.github.com>
2023-06-16 21:47:45 -0300

    Update aula9.js

--- aula9.js
+++ aula9.js
@@ -24,3 +24,54 @@ Media dos demais saltos: 5.9 m
 
 Resultado final:
 Rodrigo Curvêllo: 5.9 m
+*/
+
+var prompt = require('prompt-sync')();
+
+//variaveis nome e valores dos saltos.
+let nome = prompt('Atleta: ');
+let vetor = [];
+let i = 0;
+let salto = 0;
+
+do{
+    salto = Number(prompt(`salto ${i+1}: `));
+    vetor[i] = salto;
+    i++;
+}while(salto != vetor[4]);
+
+//variaveis do maior valor e menor valor do vetor.
+let maiorSalto = 0;
+let menorSalto = 0;
+let posicaoMaior = vetor[0];
+let posicaoMenor = vetor[0];
+
+for(let j=0; j<vetor.length; j++){
+    
+    if(vetor[j]>maiorSalto){
+        
+        maiorSalto = vetor[j];
+        posicaoMaior = j;
+    }
+
+    for(let n=0; n<vetor.length; n++){
+    
+    if(vetor[n]<vetor[j]){
+        
+        menorSalto = vetor[n];
+        posicaoMenor = n;
+    }
+}
+
+}
+//variaveis que excluem o menor e maior valor do vetor. Calculo da média. 
+const excluirMenor = vetor.splice(posicaoMenor, 1);
+const excluirMaior = vetor.splice(posicaoMaior, 1);
+const calculo = (vetor[0]+vetor[1]+vetor[2])/3;
+
+//Finalização.
+console.log(`Melhor salto: ${maiorSalto} m`);
+console.log(`Pior salto: ${menorSalto} m`);
+console.log(`média dos demais saltos: ${calculo} m`);
+console.log('resultado final:');
+console.log(`${nome}: ${calculo} m`);
